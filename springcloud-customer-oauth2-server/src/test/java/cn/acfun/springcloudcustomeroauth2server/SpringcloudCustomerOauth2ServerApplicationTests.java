fa76fba020d4bd8c126313f5ef84ff60b15de837
goldhadis <goldhadis@gmail.com>
2021-09-04 00:07:10 +0800

    springcloud学习 wk

---
+++ springcloud-customer-oauth2-server/src/test/java/cn/acfun/springcloudcustomeroauth2server/SpringcloudCustomerOauth2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.acfun.springcloudcustomeroauth2server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudCustomerOauth2ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
